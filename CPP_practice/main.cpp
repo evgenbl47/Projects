9280a5bd8cb687ee499e2ebf3b069486f8474b9d
evgenbl47 <evgenbl47@gmail.com>
2024-09-13 00:44:42 +0500

    try for

--- CPP_practice/main.cpp
+++ CPP_practice/main.cpp
@@ -2,10 +2,17 @@
 
 using namespace std;
 main(){
+  setlocale(LC_ALL, "rus");
+
+  int sum = 0;
+  for(int i = 0; i < 10; i++){
+    sum += 1;
+  }
+
   cout << "Hello World" << endl;
-  cout << "3 + 2 =" << 3 + 2 << endl;
+  cout << "sum:" << sum << endl;
+
 
   system("pause");
-
 
 }
