f58abb1f150fa3815a82fe4d63ba48fd34db04fa
evgenbl47 <evgenbl47@gmail.com>
2024-09-16 21:42:25 +0500

    Change 3 numbers by circle

--- CPP_practice/main.cpp
+++ CPP_practice/main.cpp
@@ -4,19 +4,55 @@ using namespace std;
 main(){
   setlocale(LC_ALL, "rus");
 
-  int num1 = 0;
-  int num2 = 0;
+  int first = 0;
+  int second = 0;
+  int third = 0;
   int tmp = 0;
 
-  cout << "Enter two numbers : ";
-  cin >> num1 >> num2;
+  cout << "Type any integer : ";
+  first = 0;
+  //cin >> first;
+  cout << "type another number : ";
+  second = 1;
+  //cin >> second;
+  cout << "type third number : " << endl;
+  third = 2;
+  //cin >> third;
 
-  cout << "Before reverse : " << num1 << " - num1, " << num2 << " - num2" << endl;
-  tmp = num1;
-  num1 = num2;
-  num2 = tmp;
-  cout << "After reverse : " << num1 << " - num1, " << num2 << " - num2";
+  cout << "You typed : " << first 
+  << " - first number," << second 
+  << " - second number," << third 
+  << " - third number" << endl;
 
+  tmp = third;
+  third = second;
+  second = first;
+  first = tmp;
+
+  cout << "Numbers after slide : " << first 
+  << " - first number, " << second 
+  << " - second number, " << third 
+  << " - third number." << endl;
+
+  tmp = third;
+  third = second;
+  second = first;
+  first = tmp;
+
+  cout << "Numbers after second slide : " << first 
+  << " - first number, " << second 
+  << " - second number, " << third 
+  << " - third number." << endl;
+
+  tmp = third;
+  third = second;
+  second = first;
+  first = tmp;
+
+  cout << "Numbers after third slide : " << first 
+  << " - first number, " << second 
+  << " - second number, " << third 
+  << " - third number." << endl;
 
   cout << endl;
   system("pause");
