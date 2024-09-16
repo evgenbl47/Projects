6c591aca49ed32a3b80543ac224d6c3cf9b37fe3
evgenbl47 <evgenbl47@gmail.com>
2024-09-16 23:57:19 +0500

    extract entrie and remain

--- CPP_practice/main.cpp
+++ CPP_practice/main.cpp
@@ -6,24 +6,19 @@ using namespace std;
 main(){
   setlocale(LC_ALL, "rus");
 
-  //int tmp = 0;
-  int aSide;
-  int bSide;
-  int res;
-  int sqare = 5;
 
-  cout << "Enter a side" << endl;
-  cin >> aSide;
-  cout << "Enter a side" << endl;
-  cin >> bSide;
+  int sqare = 3;  //3 X 3
+  int userInput;
 
-  aSide = aSide / sqare;
-  bSide = bSide / sqare;
-  res = aSide * bSide;
+  cout << "Enter number" << endl;
+  cin >> userInput;
 
-  cout << "You can cut " << res << " sqare";
+  int entire = userInput / sqare;
+  int remain = userInput - entire;
 
+  cout << "You can get " << entire << "." << remain << endl;
 
+ 
   cout << endl;
   system("pause");
-} 
+}
