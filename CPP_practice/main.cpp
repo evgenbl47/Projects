f11f28e6f0b113397916c306c80f7a022dd2b982
evgenbl47 <evgenbl47@gmail.com>
2024-09-17 22:30:57 +0500

    Clock v2 calculations!

--- CPP_practice/main.cpp
+++ CPP_practice/main.cpp
@@ -7,18 +7,19 @@ main(){
   setlocale(LC_ALL, "rus");
 
   //  /10  cut last num, /100 cut last two num ...exp, %10 get last num
-  int userInput;
+
+  int userSec;
+  int res;
 
   cout << "type seconds : ";
-  cin >> userInput;
+  cin >> userSec;
 
-  int sec;
-  int min;
-
-  min = userInput / 60 % 60;
-  sec = userInput % 60;
- 
-  cout << "min:" << min << " sec :" << sec;
+  res = userSec / 3600 % 24;
+  cout << "Houer: " << res << endl;
+  res = userSec % 3600 / 60;
+  cout << "Munutes: " << res << endl;
+  res = userSec % 60;
+  cout << "Second: " << res << endl;
  
   cout << endl;
   system("pause");
