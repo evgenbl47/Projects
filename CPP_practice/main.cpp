229f33f7f54e9045e08cd1da95c9b49fad250c66
evgenbl47 <evgenbl47@gmail.com>
2024-09-17 23:40:53 +0500

    clock v3 "hh:mm:ss"

--- CPP_practice/main.cpp
+++ CPP_practice/main.cpp
@@ -18,19 +18,18 @@ main(){
   //  2   /   10  =  2
   //
   int userSec;
-  int res;
+
+  int h;
+  int m;
+  int s;
 
   cout << "type seconds : ";
   cin >> userSec;
 
-  res = userSec / 3600 % 24 / 10;
-  cout << res;
-  res = userSec / 3600 % 24 % 10;
-  cout << res << ":";
-  res = userSec % 3600 / 60;
-  cout << res << ":";
-  res = userSec % 60;
-  cout << res << "." << endl;
+  h = userSec / 3600 % 24;
+  m = userSec % 3600 / 60;
+  s = userSec % 60;
+  cout << h / 10 << h % 10 << ":" << m / 10 << m % 10<< ":" << s / 10 << s % 10 << endl;
  
   cout << endl;
   system("pause");
