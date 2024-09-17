4a45c43c66d2b471837199366c4a75a41d3d843e
evgenbl47 <evgenbl47@gmail.com>
2024-09-18 00:37:44 +0500

    bool

--- CPP_practice/main.cpp
+++ CPP_practice/main.cpp
@@ -1,36 +1,24 @@
 //Created by Nt4H
 #include <iostream>
 
+//     ==
+//     !=
+//     >
+//      <
+//     >=
+//     <==
+
 
 using namespace std;
 main(){
   setlocale(LC_ALL, "rus");
 
-  //  /10  cut last num, /100 cut last two num ...exp, %10 get last num
-  // https://youtu.be/9TLC4qfFzHQ?list=PLlxYwLnXeoE0qsuXwsUi3zSU6FKOS8YCB&t=115
-  //
-  //  12  ->  1   :  2
-  //  12  /   10  =  1
-  //  12  %   10  =  2
-  //
-  //  2   ->  0   |  2        /   9  ->  0 | 9       /         14 ->  1 | 4)
-  //  2   /   10  =  0
-  //  2   /   10  =  2
-  //
-  int userSec;
+  5 == 2  0
+  3 != 4  1
+  7 != 7  0
+  6 >  6  0
+  8 <  4  0
+  5 <= 5  1
 
-  int h;
-  int m;
-  int s;
-
-  cout << "type seconds : ";
-  cin >> userSec;
-
-  h = userSec / 3600 % 24;
-  m = userSec % 3600 / 60;
-  s = userSec % 60;
-  cout << h / 10 << h % 10 << ":" << m / 10 << m % 10<< ":" << s / 10 << s % 10 << endl;
- 
-  cout << endl;
   system("pause");
 }
