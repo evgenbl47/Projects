28da159ce74564c538cfac8e5bcb7ba11ef9c0a6
evgenbl47 <evgenbl47@gmail.com>
2024-09-17 23:29:50 +0500

    clock v3 "2 -> 0 2"

--- CPP_practice/main.cpp
+++ CPP_practice/main.cpp
@@ -7,19 +7,30 @@ main(){
   setlocale(LC_ALL, "rus");
 
   //  /10  cut last num, /100 cut last two num ...exp, %10 get last num
-
+  // https://youtu.be/9TLC4qfFzHQ?list=PLlxYwLnXeoE0qsuXwsUi3zSU6FKOS8YCB&t=115
+  //
+  //  12  ->  1   :  2
+  //  12  /   10  =  1
+  //  12  %   10  =  2
+  //
+  //  2   ->  0   |  2        /   9  ->  0 | 9       /         14 ->  1 | 4)
+  //  2   /   10  =  0
+  //  2   /   10  =  2
+  //
   int userSec;
   int res;
 
   cout << "type seconds : ";
   cin >> userSec;
 
-  res = userSec / 3600 % 24;
-  cout << "Houer: " << res << endl;
+  res = userSec / 3600 % 24 / 10;
+  cout << res;
+  res = userSec / 3600 % 24 % 10;
+  cout << res << ":";
   res = userSec % 3600 / 60;
-  cout << "Munutes: " << res << endl;
+  cout << res << ":";
   res = userSec % 60;
-  cout << "Second: " << res << endl;
+  cout << res << "." << endl;
  
   cout << endl;
   system("pause");
