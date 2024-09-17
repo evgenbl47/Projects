1e5dcbe282e4727704b26ef8f53c89a4d920d1d8
evgenbl47 <evgenbl47@gmail.com>
2024-09-17 20:41:07 +0500

    sum of 4 numbers

--- CPP_practice/main.cpp
+++ CPP_practice/main.cpp
@@ -1,3 +1,4 @@
+//Created by Nt4H
 #include <iostream>
 
 
@@ -5,17 +6,14 @@ using namespace std;
 main(){
   setlocale(LC_ALL, "rus");
 
-  //  /10  cut last num, %10 get last num
+  //  /10  cut last num, /100 cut last two num ...exp, %10 get last num
   int userInput;
   int res;
 
-  cout << "Enter number" << endl;
+  cout << "Enter 4 number" << endl;
   cin >> userInput;
 
-  //res = userInput % 10;
-  //res = res + (userInput = userInput / 10) % 10;
-  //res = res + (userInput = userInput / 10) % 10;
-  res = (userInput % 10) + ((userInput = userInput / 10) % 10) + ((userInput = userInput / 10) % 10);
+  res = (userInput % 10) + ((userInput / 10) % 10) + ((userInput / 100) % 10)+ ((userInput / 1000) % 10);
 
   cout << res;
   cout << endl;
